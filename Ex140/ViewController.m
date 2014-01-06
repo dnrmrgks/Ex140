c08499451464327d1ad68d2c5e5984e18ef7cc29
park sung chan <dnrmrgks@naver.com>
2014-01-06 15:52:09 +0900

    Ex140p
    140

--- Ex140/ViewController.m
+++ Ex140/ViewController.m
@@ -9,10 +9,55 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+-(void)stopTimer;
+-(void)startTimer;
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    NSTimer *timer;
+    BOOL timerWorking;
+}
+
+
+-(void)tickDown: (NSTimer *)timer{
+    _datePicker.countDownDuration = _datePicker.countDownDuration - 60;
+    
+    if(_datePicker.countDownDuration <= 60){
+        NSLog(@"Done!");
+        [self stopTimer];
+    }
+}
+
+-(void) startTimer{
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(tickDown:) userInfo:Nil repeats:YES];
+}
+
+
+
+-(void)stopTimer{
+    
+    
+    [timer invalidate];
+    timer = nil;
+    timerWorking = NO;
+}
+
+
+- (IBAction)toggleTimer:(id)sender {
+    
+   NSString *title = timerWorking? @"Start":@"Stop";
+    [sender setTitle:title forState:UIControlStateNormal];
+    timerWorking = !timerWorking;
+    
+    if(timerWorking)
+        
+        [self startTimer];
+    
+    else
+        [self stopTimer];
+}
 
 - (void)viewDidLoad
 {
